3a1777bddb6093bcfbc21224a02478af744a1920
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-01 21:00:19 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrew Chen on 2021-05-01.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
